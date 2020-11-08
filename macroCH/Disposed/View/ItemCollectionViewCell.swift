a73b1f048911e7230ed4bc1daaa970d4821d65b2
gilbert <gilbertgozalie@gmail.com>
2020-11-09 06:42:50 +0700

    adding dispose view

---
+++ macroCH/Disposed/View/ItemCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  ItemCollectionViewCell.swift
+//  macroCH
+//
+//  Created by Gilbert Gozalie on 11/5/20.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var collectionImage: UIImageView!
+    @IBOutlet weak var collectionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+//        setup()
+    }
+    
+//    func setup() {
+//        let view = loadViewFromNib()
+//        view.frame = bounds
+//        addSubview(view)
+//    }
+//
+//    func loadViewFromNib() -> UIView {
+//        let bundle = Bundle(for: type(of: self))
+//        let nib = UINib(nibName: "collectionItemCollectionViewCell", bundle: bundle)
+//        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+//        return view
+//    }
+}
--- macroCH/Model/model.swift
+++ macroCH/Model/model.swift
@@ -8,71 +8,72 @@
 import Foundation
 
 struct User {
-    var name: String
-    var id: String
-    var gender: Int
-    var baju: Baju
-    var size: Int
-    var badan: UkuranBadan
+    var name: String = "username"
+    var id: String = "userid"
+    var gender: Int = 1
+    var baju: Baju = .init()
+    var size: Int = 1
+    var badan: UkuranBadan = .init()
 }
 
 struct Baju {
-    var name: String
-    var id: Int
-    var action: Int
-    var measurement: UkuranBaju
-    var condition: Condition
+    var name: String = "bajuname"
+    var id: String = "bajuid"
+    var action: Int = 1
+    var measurement: UkuranBaju = .init()
+    var condition: Condition = .init()
+    var dateSaved: Date = Date()
 }
 
 struct UkuranBaju {
-    var shoulder: Int
-    var chest: Int
-    var back: Int
-    var waist: Int
-    var outstream: Int
+    var shoulder: Int = 1
+    var chest: Int = 1
+    var back: Int = 1
+    var waist: Int = 1
+    var outstream: Int = 1
 }
 
 struct UkuranBadan {
-    var shoulder: Int
-    var chest: Int
-    var back: Int
-    var waist: Int
-    var outstream: Int
+    var shoulder: Int = 1
+    var chest: Int = 1
+    var back: Int = 1
+    var waist: Int = 1
+    var outstream: Int = 1
 }
 
 struct Condition {
-    var hole: Bool
-    var wearable: Bool
+    var hole: Bool = false
+    var wearable: Bool = false
 }
 
 struct DonationRecieve {
-    var name: String
-    var id: String
-    var location: Location
-    var desc: String
-    var phone: String
-    var needs: Needs
+    var name: String = "DonationRecieveName"
+    var id: String = "DonationRecieveID"
+    var location: Location = .init()
+    var desc: String = "DonationRecieve"
+    var phone: String = "01234567890"
+    var needs: Needs = .init()
 }
 
 struct Location {
-    var country: String
-    var city: String
-    var province: String
-    var postalcode: String
-    var address: String
-    var subDistrict: String
-    var note: String
+    var country: String = "Location"
+    var city: String = "Location"
+    var province: String = "Location"
+    var postalcode: String = "Location"
+    var address: String = "Location"
+    var subDistrict: String = "Location"
+    var note: String = "Location"
 }
 
 struct Needs {
-    var item: Int
-    var desc: String
-    var purpose: String
+    var item: Int = 1
+    var desc: String  = "Location"
+    var purpose: String = "Location"
 }
 
 struct News {
-    var title: String
-    var details: String
-    var content: String
-    var date: Date
+    var title: String = "News"
+    var details: String = "News"
+    var content: String = "News"
+    var date: Date = .init()
 }
